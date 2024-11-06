82443a1793cb64518270a2f4a279ec3a07378130
Vidura Fernando <heyvidura@gmail.com>
2024-11-06 22:32:20 +0530

    feat: task tracker

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,11 +1,65 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+#[derive(Debug)]
+enum TaskStatus {
+    Todo,
+    InProgress,
+}
+
+#[derive(Debug)]
+struct Task {
+    id: u32,
+    description: String,
+    status: TaskStatus,
+}
+
+struct TaskTracker {
+    tasks: Vec<Task>,
+    next_id: u32,
+}
+
+impl TaskTracker {
+    fn new() -> Self {
+        TaskTracker {
+            tasks: Vec::new(),
+            next_id: 1,
+        }
+    }
+
+    fn add_task(&mut self, description: String) {
+        let task = Task {
+            id: self.next_id,
+            description,
+            status: TaskStatus::Todo,
+        };
+        self.tasks.push(task);
+        self.next_id += 1;
+    }
+
+    fn update_task_status(&mut self, id: u32, new_status: TaskStatus) -> Result<(), String> {
+        if let Some(task) = self.tasks.iter_mut().find(|t| t.id == id) {
+            task.status = new_status;
+            Ok(())
+        } else {
+            Err(format!("Task with id {} not found", id))
+        }
+    }
+
+    fn list_tasks(&self) {
+        for task in &self.tasks {
+            println!(
+                "ID: {}, Description: {}, Status: {:?}",
+                task.id, task.description, task.status
+            );
+        }
+    }
+}
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    let mut tracker = TaskTracker::new();
+    tracker.add_task("Task one".to_string());
+    tracker.add_task("Task two".to_string());
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    tracker
+        .update_task_status(1, TaskStatus::InProgress)
+        .unwrap();
+    tracker.list_tasks();
 }
